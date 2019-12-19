c3726f8215e37eca4ba4439ee450de9272af8e73
mohamed awad <awadmohamed233@gmail.com>
2019-12-20 00:18:08 +0200

    add appId and fix issues

--- backend/src/lambda/auth/auth0Authorizer.ts
+++ backend/src/lambda/auth/auth0Authorizer.ts
@@ -1,10 +1,10 @@
 import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
-
-import { verify, decode } from 'jsonwebtoken'
+// import { decode } from 'jsonwebtoken'
+import { verify } from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger'
 // import Axios from 'axios'
-import { Jwt } from '../../auth/Jwt'
+// import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 
 const logger = createLogger('auth')
@@ -58,7 +58,7 @@ const authSecret = process.env.AUTH_0_SECRET
 
 async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
-  const jwt: Jwt = decode(token, { complete: true }) as Jwt
+  // const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
   // TODO: Implement token verification
   // You should implement it similarly to how it was implemented for the exercise for the lesson 5
--- backend/src/lambda/http/getTodos.ts
+++ backend/src/lambda/http/getTodos.ts
@@ -2,23 +2,32 @@ import 'source-map-support/register'
 import { APIGatewayProxyEvent, APIGatewayProxyResult, APIGatewayProxyHandler } from 'aws-lambda'
 import * as AWS from 'aws-sdk'
 import 'source-map-support/register'
+import { parseUserId } from '../../auth/utils'
 
 const documentClient = new AWS.DynamoDB.DocumentClient()
 const todosTable = process.env.TODOS_TABLE
 
 export const handler: APIGatewayProxyHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   // TODO: Get all TODO items for a current user
+  const authHeader = event.headers.Authorization
+  const authSplit = authHeader.split(" ")
+  const token = authSplit[1]
+
   const todos = await documentClient.scan({
-    TableName: todosTable
+    TableName: todosTable,
+    FilterExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': parseUserId(token),
+    },
   }).promise()
   const items = todos.Items
   return {
-      statusCode: 200,
-      headers: {
-          'Access-Control-Allow-Origin': '*'
-      },
-      body: JSON.stringify({
-          items
-      })
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify({
+      items
+    })
   }
 }
